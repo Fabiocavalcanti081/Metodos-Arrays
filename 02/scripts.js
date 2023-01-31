4be5680e13e31cd54a5bb53358ee57a49025d4fb
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-31 15:54:49 -0300

    Exercício 02 Divisão de Grupos Finalizado

--- 02/scripts.js
+++ 02/scripts.js
@@ -2,16 +2,11 @@ const nomes = ['Juninho', 'Vidal', 'Guido', 'Dani', 'Ruli', 'Diego'];
 const tamanhoDoGrupo = 4;
 
 function divisaoGrupos(nomes) {
-    let quantidade = Math.abs(tamanhoDoGrupo - nomes.length);
-    let i = 0;
-    let resto = [];
+    let grupo1 = nomes.slice(0, tamanhoDoGrupo);
+    let grupo2 = nomes.slice(tamanhoDoGrupo);
+    grupo1 = grupo1.join(", ");
+    grupo2 = grupo2.join(", ");
 
-    while (i < quantidade) {
-        resto.unshift(nomes.pop());
-        i++
-    }
-    const grupo1 = nomes.join(", ");
-    const grupo2 = resto.join(", ")
     console.log(`Grupo 1: ${grupo1}.`);
     console.log(`Grupo 2: ${grupo2}.`);
 }
