535bf814b734e02a6f26384e39ffceacd33b2d5d
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-31 15:59:33 -0300

    Exercício 03 Grupos de Carros Finalizado

--- 03/scripts.js
+++ 03/scripts.js
@@ -1,18 +1,11 @@
 const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
 const posicao = 3;
 
-function gruposCarros(nomes, posicao) {
-    let i = posicao;
-    let resultado = posicao + 3;
-    let carros = [];
-
-    while (i < resultado) {
-        carros.push(nomes[i]);
-        i++
-    }
-
+function gruposCarros(nomes) {
+    let carros = nomes.slice(posicao, posicao + 3);
     carros = carros.join(" - ");
+
     console.log(carros);
 }
 
-gruposCarros(nomes, posicao);
+gruposCarros(nomes);
