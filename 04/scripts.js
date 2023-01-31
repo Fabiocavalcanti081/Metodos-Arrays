e2e7735f2e2977d8bdd5c26a1d212fc6cef57582
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-31 17:40:39 -0300

    Exercício 04 Fila de atendimento Finalizado

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,13 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function filaAtendimento(pacientes, operacao, nome) {
+    if (operacao === "agendar") {
+        pacientes.push(nome);
+    } else if (operacao === "atender") {
+        pacientes.shift();
+    }
+    let lista = pacientes.join(", ");
+    console.log(lista);
+}
+
+filaAtendimento(pacientes, "agendar", "Fábio");
